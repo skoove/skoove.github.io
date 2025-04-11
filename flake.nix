ff09cf941f30e4405c2f2a63ec859dd1fb3fd56e
Zie <53106860+skoove@users.noreply.github.com>
2025-04-11 10:30:03 +1000

    flake: init

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Jekyll development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ruby
+            bundler
+            jekyll
+            nodejs # Sometimes needed for Jekyll plugins
+            
+            # Common dependencies
+            gcc
+            gnumake
+            libffi
+            zlib
+          ];
+        };
+      });
+}
